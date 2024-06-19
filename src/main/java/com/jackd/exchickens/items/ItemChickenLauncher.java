8e983de207d213dbc6a5d8b404b550e2318e7e26
Jack Davenport <jack.davenport5011@gmail.com>
2024-06-20 02:42:02 +1000

    Play sound when firing launcher

--- src/main/java/com/jackd/exchickens/items/ItemChickenLauncher.java
+++ src/main/java/com/jackd/exchickens/items/ItemChickenLauncher.java
@@ -8,6 +8,8 @@ import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ProjectileItem;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.Direction;
@@ -23,6 +25,8 @@ public class ItemChickenLauncher extends Item implements ProjectileItem {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack stack = user.getStackInHand(hand);
+
+        world.playSound((PlayerEntity)null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_GENERIC_EXPLODE.value(), SoundCategory.PLAYERS);
 
         if(!world.isClient) {
             ItemStack fired = new ItemStack(ModContent.TRICK_EGG_ITEM);
