29ccc6c3bbc1ee1ff544fe5bd8a8988515913fd4
Jack Davenport <jack.davenport5011@gmail.com>
2024-06-22 16:11:13 +1000

    Spawn smoke particles when firing launcher

--- src/client/java/com/jackd/exchickens/client/mixins/PlayerEntityRendererMixin.java
+++ src/client/java/com/jackd/exchickens/client/mixins/PlayerEntityRendererMixin.java
@@ -1,7 +1,5 @@
 package com.jackd.exchickens.client.mixins;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
--- src/main/java/com/jackd/exchickens/items/ItemChickenLauncher.java
+++ src/main/java/com/jackd/exchickens/items/ItemChickenLauncher.java
@@ -8,12 +8,15 @@ import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ProjectileItem;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Position;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class ItemChickenLauncher extends Item implements ProjectileItem {
@@ -34,6 +37,7 @@ public class ItemChickenLauncher extends Item implements ProjectileItem {
                 egg.setVelocity(user, user.getPitch(), user.getYaw(), 0f, 3.5f, 1f);
                 world.spawnEntity(egg);
             }
+            spawnSmokeParticles(user, world);
             consumeAmmo(user, ModContent.TRICK_EGG_ITEM, 1);
             return TypedActionResult.success(stack, false);
         } else {
@@ -50,6 +54,17 @@ public class ItemChickenLauncher extends Item implements ProjectileItem {
                 stack.decrementUnlessCreative(quantity, player);
                 return;
             }
+        }
+    }
+
+    private void spawnSmokeParticles(PlayerEntity player, World world) {
+        Vec3d eyePos = player.getEyePos();
+        float f = -MathHelper.sin(player.getYaw() * 0.017453292F) * MathHelper.cos(player.getPitch() * 0.017453292F);
+        float g = -MathHelper.sin(player.getPitch() * 0.017453292F);
+        float h = MathHelper.cos(player.getYaw() * 0.017453292F) * MathHelper.cos(player.getPitch() * 0.017453292F);
+        for(int i = 0; i < 16; i++) {
+            float s = (float)(i + 1) / 8f;
+            world.addParticle(ParticleTypes.SMOKE, eyePos.x, eyePos.y, eyePos.z, f * s, g * s, h * s);
         }
     }
 
