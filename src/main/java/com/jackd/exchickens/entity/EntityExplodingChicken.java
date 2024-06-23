c7a2b399ae95e1274c1ecb70ea8374abb6329eb4
Jack Davenport <jack.davenport5011@gmail.com>
2024-06-24 00:11:27 +1000

    Allow attachment of fireworks onto chicken, spawn fireworks on explode

--- src/main/java/com/jackd/exchickens/entity/EntityExplodingChicken.java
+++ src/main/java/com/jackd/exchickens/entity/EntityExplodingChicken.java
@@ -1,10 +1,15 @@
 package com.jackd.exchickens.entity;
 
+import java.util.List;
+
 import org.jetbrains.annotations.Nullable;
 
 import com.jackd.exchickens.ModContent;
 import com.jackd.exchickens.util.ItemUtils;
 
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.FireworkExplosionComponent;
+import net.minecraft.component.type.FireworksComponent;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
@@ -22,12 +27,21 @@ import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.World.ExplosionSourceType;
 
 public class EntityExplodingChicken extends ChickenEntity {
 
     private static final TrackedData<ItemStack> FIREWORK_STACK;
+    private static final TrackedData<Boolean> FIREWORK_IGNITED;
+    private static final byte STATUS_FIREWORK_EXPLODE = 17;
 
     private static final float MIN_EXPLODE_RANGE = 2.0F;
     private static final float MAX_EXPLODE_RANGE = 6.0F;
@@ -43,6 +57,7 @@ public class EntityExplodingChicken extends ChickenEntity {
     protected void initDataTracker(Builder builder) {
         super.initDataTracker(builder);
         builder.add(FIREWORK_STACK, getDefaultFireworkStack());
+        builder.add(FIREWORK_IGNITED, false);
     }
 
     public boolean hasFireworkAttached() {
@@ -60,11 +75,27 @@ public class EntityExplodingChicken extends ChickenEntity {
         return true;
     }
 
+    public void removeFirework() {
+        this.dataTracker.set(FIREWORK_STACK, getDefaultFireworkStack());
+    }
+
+    public void igniteFirework() {
+        if(this.hasFireworkAttached()) {
+            this.getWorld().playSound((PlayerEntity)null, this.getX(), this.getY(), this.getZ(), SoundEvents.ENTITY_FIREWORK_ROCKET_LAUNCH, SoundCategory.AMBIENT, 3.0F, 1.0F);
+            this.dataTracker.set(FIREWORK_IGNITED, true);
+        }
+    }
+
     @Override
     public void tick() {
         super.tick();
         if(this.willExplode) {
             this.explode();
+        }
+        if(this.hasFireworkAttached() && this.dataTracker.get(FIREWORK_IGNITED)) {
+            float offX = -MathHelper.sin(this.getYaw() * 0.017453292F);
+            float offZ = MathHelper.cos(this.getYaw() * 0.017453292F);
+            this.getWorld().addParticle(ParticleTypes.FIREWORK, this.getX() + offX, this.getY() + 0.5, this.getZ() + offZ, 0d, 0d, 0d);
         }
     }
 
@@ -75,7 +106,19 @@ public class EntityExplodingChicken extends ChickenEntity {
         World world = this.getWorld();
         if(!world.isClient()) {
             world.createExplosion(this, this.getX(), this.getY(), this.getZ(), randomExplosionRange(), ExplosionSourceType.MOB);
+            this.getWorld().sendEntityStatus(this, STATUS_FIREWORK_EXPLODE);
             this.discard();
+        }
+    }
+
+    private void explodeFireworks() {
+        if(!this.hasFireworkAttached()) return;
+        ItemStack itemStack = this.getFireworkStack();
+        FireworksComponent fireworksComponent = (FireworksComponent)itemStack.get(DataComponentTypes.FIREWORKS);
+        if(fireworksComponent != null) {
+            List<FireworkExplosionComponent> explosions = fireworksComponent.explosions();
+            Vec3d velocity = this.getVelocity();
+            this.getWorld().addFireworkParticle(this.getX(), this.getY(), this.getZ(), velocity.x, velocity.y, velocity.z, explosions);
         }
     }
 
@@ -95,6 +138,41 @@ public class EntityExplodingChicken extends ChickenEntity {
         }
 
         return true;
+    }
+
+    @Override
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+        // is the player holding a firework rocket?
+        ItemStack heldItem = player.getStackInHand(hand);
+        if(heldItem != null && heldItem.isOf(Items.FIREWORK_ROCKET)) {
+            // check if chicken already has a rocket attached
+            ItemStack oldStack = null;
+            if(this.hasFireworkAttached()) {
+                oldStack = this.getFireworkStack();
+            }
+            // set new firework rocket, decrease stack size
+            this.setFireworkStack(heldItem.copyWithCount(1));
+            heldItem.decrementUnlessCreative(1, player);
+            // give old rocket back to player
+            if(oldStack != null) {
+                player.giveItemStack(oldStack);
+            }
+            return ActionResult.SUCCESS;
+        // otherwise is there a rocket attached?
+        } else if(this.hasFireworkAttached()) {
+            // if clicking with a flint and steel, ignite rocket
+            if(heldItem.isOf(Items.FLINT_AND_STEEL)) {
+                this.igniteFirework();
+                return ActionResult.CONSUME;
+            }
+            
+            // give rocket to player and remove from chicken
+            player.giveItemStack(this.getFireworkStack());
+            this.removeFirework();
+            return ActionResult.SUCCESS_NO_ITEM_USED;
+        }
+        // otherwise use vanilla behaviour
+        return super.interactMob(player, hand);
     }
 
     @Override
@@ -120,6 +198,7 @@ public class EntityExplodingChicken extends ChickenEntity {
     @Override
     public void readCustomDataFromNbt(NbtCompound nbt) {
         super.readCustomDataFromNbt(nbt);
+        this.dataTracker.set(FIREWORK_IGNITED, nbt.getBoolean("FireworkIgnited"));
         if(nbt.contains("Firework", 10)) {
             ItemStack stack = ItemStack.fromNbt(this.getRegistryManager(), nbt.getCompound("Firework")).orElse(getDefaultFireworkStack());
             this.dataTracker.set(FIREWORK_STACK, stack);
@@ -131,7 +210,16 @@ public class EntityExplodingChicken extends ChickenEntity {
         super.writeCustomDataToNbt(nbt);
         if(this.hasFireworkAttached()) {
             nbt.put("Firework", this.getFireworkStack().encode(getRegistryManager()));
+            nbt.putBoolean("FireworkIgnited", this.dataTracker.get(FIREWORK_IGNITED));
         }
+    }
+
+    @Override
+    public void handleStatus(byte status) {
+        if(status == STATUS_FIREWORK_EXPLODE && this.getWorld().isClient) {
+            this.explodeFireworks();
+        }
+        super.handleStatus(status);
     }
 
     private static ItemStack getDefaultFireworkStack() {
@@ -140,6 +228,7 @@ public class EntityExplodingChicken extends ChickenEntity {
 
     static {
         FIREWORK_STACK = DataTracker.registerData(EntityExplodingChicken.class, TrackedDataHandlerRegistry.ITEM_STACK);
+        FIREWORK_IGNITED = DataTracker.registerData(EntityExplodingChicken.class, TrackedDataHandlerRegistry.BOOLEAN);
     }
 
 }
