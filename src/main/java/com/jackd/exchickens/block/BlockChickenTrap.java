fd6219649b20511b3402c870df2037970bc0f08a
Jack Davenport <jack.davenport5011@gmail.com>
2024-07-13 15:24:13 +1000

    Add custom outline shape for trap block

--- src/main/java/com/jackd/exchickens/block/BlockChickenTrap.java
+++ src/main/java/com/jackd/exchickens/block/BlockChickenTrap.java
@@ -6,11 +6,16 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager.Builder;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.World.ExplosionSourceType;
 
@@ -21,6 +26,8 @@ public class BlockChickenTrap extends HorizontalFacingBlock {
 
     public static final MapCodec<BlockChickenTrap> CODEC = createCodec(BlockChickenTrap::new);
     public static final BooleanProperty ACTIVE = BooleanProperty.of("active");
+    private static final VoxelShape SHAPE_Z;
+    private static final VoxelShape SHAPE_X;
 
     public BlockChickenTrap(Settings settings) {
         super(settings);
@@ -54,18 +61,39 @@ public class BlockChickenTrap extends HorizontalFacingBlock {
     }
 
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+        return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing());
     }
 
     @Override
     protected void appendProperties(Builder<Block, BlockState> builder) {
         super.appendProperties(builder);
-        builder.add(HorizontalFacingBlock.FACING, ACTIVE);
+        builder.add(FACING, ACTIVE);
+    }
+
+    @Override
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        switch(state.get(FACING)) {
+            case NORTH:
+            case SOUTH:
+                return SHAPE_Z;
+            case EAST:
+            case WEST:
+            default:
+                return SHAPE_X;
+        }
     }
 
     @Override
     protected MapCodec<? extends HorizontalFacingBlock> getCodec() {
         return CODEC;
+    }
+
+    static {
+        VoxelShape base = Block.createCuboidShape(0d, 0d, 0d, 16d, 2d, 16d);
+        VoxelShape armsZ = Block.createCuboidShape(0d, 2d, 7d, 16d, 14d, 9d);
+        VoxelShape armsX = Block.createCuboidShape(7d, 2d, 0d, 9d, 14d, 16d);
+        SHAPE_Z = VoxelShapes.combine(base, armsZ, BooleanBiFunction.OR);
+        SHAPE_X = VoxelShapes.combine(base, armsX, BooleanBiFunction.OR);
     }
 
 }
