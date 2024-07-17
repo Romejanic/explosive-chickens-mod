0f18cc3d9370a43fbd3f0d10a58bf12fabe45412
Jack Davenport <jack.davenport5011@gmail.com>
2024-07-17 18:42:48 +1000

    Refactor explosive armor logic into separate class
    Improve readability of mixin code and better show intent

---
+++ src/main/java/com/jackd/exchickens/armor/ExplosiveChickenArmor.java
@@ -0,0 +1,35 @@
+package com.jackd.exchickens.armor;
+
+import com.jackd.exchickens.ModContent;
+import com.jackd.exchickens.util.ExplosionSizes;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.world.World;
+import net.minecraft.world.World.ExplosionSourceType;
+
+public class ExplosiveChickenArmor {
+
+    public static void handleEntityDeath(Entity entity, World world, Iterable<ItemStack> iterable) {
+        if(!world.isClient) {
+            float multiplier = 0f;
+            for(ItemStack armorStack : iterable) {
+                if(armorStack.getItem() instanceof ArmorItem armorItem && isChickenArmor(armorItem.getMaterial())) {
+                    multiplier += armorItem.getType().getMaxDamage(1) / 10.0f;
+                }
+            }
+            if(multiplier > 0f) {
+                float explosionPower = ExplosionSizes.chickenArmor() * multiplier;
+                world.createExplosion(entity, entity.getX(), entity.getY(), entity.getZ(), explosionPower, ExplosionSourceType.MOB);
+            }
+        }
+    }
+
+    private static boolean isChickenArmor(RegistryEntry<ArmorMaterial> material) {
+        return material == ModContent.CHICKEN_ARMOR;
+    }
+
+}
--- src/main/java/com/jackd/exchickens/mixins/LivingEntityMixin.java
+++ src/main/java/com/jackd/exchickens/mixins/LivingEntityMixin.java
@@ -6,17 +6,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.jackd.exchickens.ModContent;
-import com.jackd.exchickens.util.ExplosionSizes;
+import com.jackd.exchickens.armor.ExplosiveChickenArmor;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.world.World;
-import net.minecraft.world.World.ExplosionSourceType;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
@@ -28,22 +23,7 @@ public abstract class LivingEntityMixin extends Entity {
     @Inject(method="dropInventory", at=@At("HEAD"))
     private void dropInventory(CallbackInfo info) {
         World world = this.getWorld();
-        if(!world.isClient) {
-            float multiplier = 0f;
-            for(ItemStack armorStack : this.getArmorItems()) {
-                if(armorStack.getItem() instanceof ArmorItem armorItem && this.isChickenArmor(armorItem.getMaterial())) {
-                    multiplier += armorItem.getType().getMaxDamage(1) / 10.0f;
-                }
-            }
-            if(multiplier > 0f) {
-                float explosionPower = ExplosionSizes.chickenArmor() * multiplier;
-                world.createExplosion(this, this.getX(), this.getY(), this.getZ(), explosionPower, ExplosionSourceType.MOB);
-            }
-        }
-    }
-
-    private boolean isChickenArmor(RegistryEntry<ArmorMaterial> material) {
-        return material == ModContent.CHICKEN_ARMOR;
+        ExplosiveChickenArmor.handleEntityDeath(this, world, this.getArmorItems());
     }
 
     @Shadow
